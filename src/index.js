0fcf2ced07a3c03b4c846b80c8baa88a6491437f
SOFTOV\hadar.m <macdasi@gmail.com>
2016-11-20 15:46:16 +0200

    Add orderbook call

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 var request = require('request-json');
 var client = request.createClient('https://www.bit2c.co.il/');
 
- function callTicker(){
-	 return  client.get('Exchanges/BtcNis/Ticker.json').then(function(result) {
-				return 	result.body;
-		  });
+function callTicker() {
+    return client.get('Exchanges/BtcNis/Ticker.json').then(function (result) {
+        return result.body;
+    });
 }
 
- module.exports = {
-	ticker : callTicker()
- };
+function callOrderbook() {
+    return client.get('Exchanges/BtcNis/orderbook.json').then(function (result) {
+        return result.body;
+    });
+}
+
+module.exports = {
+    ticker: callTicker() ,
+	 orderbook : callOrderbook()
+};
   
 
